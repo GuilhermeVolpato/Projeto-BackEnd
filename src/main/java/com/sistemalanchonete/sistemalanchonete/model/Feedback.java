6b3bbcd290c90509f91b34e8d3de5d82ff7fd243
paulorsimao <paulor_simao@hotmail.com>
2023-10-16 19:05:56 -0300

    Modificação para atender regra de negocio feedback

---
+++ src/main/java/com/sistemalanchonete/sistemalanchonete/model/Feedback.java
@@ -0,0 +1,14 @@
+package com.sistemalanchonete.sistemalanchonete.model;
+
+public class Feedback extends Status {
+    private String observacao;
+
+    public String getObservacao() {
+        return observacao;
+    }
+
+    public void setObservacao(String observacao) {
+        this.observacao = observacao;
+    }
+}
+
